f53c2b6f3abcf7987457072816170e14cb821553
ItzWarLockOp <98949064+ItzWarLockOp@users.noreply.github.com>
2022-07-06 13:04:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 #Logger Setup
 logger = logging.getLogger(__name__)
 
-TOKEN = "YOUR_TOKEN_HERE"
+TOKEN = "5413853569:AAHIk63yVYAzeluJXtzL4e1dhVRisvYzw8s"
 
 def download(update: Update, context: CallbackContext):
     message = update.effective_message
